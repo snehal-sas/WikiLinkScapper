4de7693cd9a6a6ba9e739ecb09fe9911352bf2fd
Snehal Shokeen <snehalshokeen@gmail.com>
2020-01-05 16:48:32 -0800

    added demo video link

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # WikiLinkScapper
 
-WikiLinkScrapper- this program is a web scraper designed to enter a word in wikipedia and follow the first x number of links to other wikipedia pages. I think it would be amusing to see where it ends up.
+WikiLinkScrapper- this program is a web scraper designed to enter a word in wikipedia
+and follow the first x numbered link to other wikipedia pages y number of times.
 
-Demo can be found here: 
+Demo can be found here: https://youtu.be/hTVn4e_vLzo
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # WikiLinkScrapper- this program is a web scraper designed to enter a word in wikipedia
-# and follow the first x number of links to other wikipedia pages.
+# and follow the first x numbered link to other wikipedia pages y number of times.
 # I think it would be amusing to see where it ends up.
 
 import requests
@@ -9,15 +9,15 @@ import random
 
 url_list = []
 # You can select desired parameter below
-# Note that " " must be enter as "_". Thus, "Steve Jobs" would be "Steve_Jobs"
+# Note that " " must be enter as "_". Thus, "Steve Ballmer" would be "Steve_Ballmer"
 Wikipage= "Steve_Ballmer"
 Number_O_Searches = 7
-Link_Number = 5 # runs into problem if below 4. Pick any number 4 or above. 
-# fetches all links on the content page of the given "Wikipage" root page.
-# "Wikipage" is a variable above
+Link_Number = 5  
+# fetches all links on the content page of the given "Wikipage" root page
 
 
 url = ("/wiki/" + Wikipage)
+Link_Number = Link_Number - 3
 for x in range(Number_O_Searches):
     r = requests.get("https://en.wikipedia.org" + url)
     b = BeautifulSoup(r.text, "html.parser")
@@ -35,9 +35,8 @@ for x in range(Number_O_Searches):
         #print(list_O_links[x])
 
     #Wikipedia seems to keep falling into a loop. Everything below is to avoid that.
-    link_number = Link_Number -3
-    url = list_O_links[link_number+1]
-    updated_link_number = link_number +1
+    url = list_O_links[Link_Number+1]
+    updated_link_number = Link_Number +1
     while True:
         if url in url_list:
             updated_link_number = updated_link_number + 1
